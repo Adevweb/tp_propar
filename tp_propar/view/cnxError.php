a61aa4a33eeea10087a17634f86f028af3a899e1
Laurie <laurie.brun@outlook.fr>
2020-02-13 12:03:54 +0100

    amélioration des vues

---
+++ tp_propar/view/cnxError.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <title>Ajouter une opération</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/v4-shims.css">
+
+    <link rel="stylesheet" href="css/cnxError.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- reference your copy Font Awesome here (from our CDN or by hosting yourself) -->
+
+</head>
+
+<body>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- HEADER -->
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-between header">
+            <h1 class="headTitleLeft"> PROPAR </h1>
+            <h5 class="headTitleRight"><span style="font-size:30px;"> <i class="fas fa-sign-out-alt"></i></span> </h5>
+        </div>
+    </div>
+    <!-- SECTION -->
+    <div class="container">
+        <div class="row col-12">
+            <h3 class="display-4 col-12 text-center title">Erreur - Utilisateur non reconnu</h3>
+            <h5 class="display-5 col-12 text-center sstitle">Veuillez retourner à l'accueil</h5>
+        </div>
+    </div>
+
+    <!-- FORMULAIRE -->
+    <div class="container justify-content-center ajouter">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+
+                <div class="display-4 col-12 text-center">
+                    <a href="connexion.php"><button type="submit" id="accueil" class="btn btn-light" value="Accueil"><span class="fa fa-home"></span> Accueil</button></a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- FIN FORMULAIRE -->
+
+
+
+</body>
+
+</html>
