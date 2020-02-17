0144ccec7f9ddb808e5aa8571d2ed6f295ca8881
Laurie <laurie.brun@outlook.fr>
2020-02-17 11:31:19 +0100

    hgfd

--- tp_propar/view/addClient.php
+++ tp_propar/view/addClient.php
@@ -44,10 +44,10 @@
     <div class="row justify-content-center">
       <form action="../controler/addClient_action.php" method="post">
         <div class="row col-12">
-          <div class="col-md-4  form-group" data-for="name">
+          <div class="col-md-6  form-group" data-for="name">
             <input type="text" class="form-control" name="nom" placeholder="Nom" required>
           </div>
-          <div class="col-md-4  form-group" data-for="prenom">
+          <div class="col-md-6  form-group" data-for="prenom">
             <input type="text" class="form-control" name="prenom" placeholder="Prenom" required>
           </div>
         </div>
