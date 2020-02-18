ba167134c7cf74784158841efc4398930da99293
Adrien <adevweb00@gmail.com>
2020-02-18 11:45:45 +0100

    Ajout des logs dans cnx.php

--- tp_propar/controler/cnx_action.php
+++ tp_propar/controler/cnx_action.php
@@ -5,6 +5,11 @@ require_once '../model/senior_class.php';
 require_once '../model/apprenti_class.php';
 require_once '../model/operation_class.php';
 
+include('../log/log4php/Logger.php');
+Logger::configure('../log/config.xml');
+$log = Logger::getLogger('CONNEXION');
+
+
 session_start();
 
 
@@ -28,13 +33,14 @@ if ($_POST) {
 
 //Si la validation des champs s'est correctement passer, on place les variables en session
 if ($validation) {
-    $_SESSION['login'] = $login;
-    $_SESSION['mdp'] = $mdp;
+    
     //On appel checkUser pour vérifier dans la BDD l'existence des logins et retourne un OBJET
     $check = Connexion::checkUser($login, $mdp);
     //Si l'objet n'est pas vide...
     if (isset($check) && !empty($check)) {
         //Récupération des attributs 
+        $_SESSION['login'] = $login;
+        $_SESSION['mdp'] = $mdp;
         $_SESSION['id_user'] = $check->get_id();
         $id_user = $_SESSION['id_user'];
         $_SESSION['type'] = $check->get_type();
@@ -51,13 +57,16 @@ if ($validation) {
     
     //Controle du type de user pour la bonne redirection
         if ($check->get_type() == "EXPERT") {
+                $log->trace("L'utilisateur $login s'est connecté");   // Not logged because TRACE < WARN
                 header('location: ../view/homeAdmin.php');
             } else {
+                $log->trace("L'utilisateur $login s'est connecté");   // Not logged because TRACE < WARN
                 header('location: ../view/homeUser.php');
             }
     }
 //Si le tableau est vide, c'est un utilisateur inconnu en BDD il est donc redirigé vers la page d'erreur
 else {
+    $log->info("L'utilisateur s'est connecté avec le mauvais Login ou MDP");    // Not logged because INFO < WARN
     header('location: ../view/cnxError.php');
     }
 }
