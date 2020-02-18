b1193e9b83f1188255e48202ccaa28a5029641ac
Adrien <adevweb00@gmail.com>
2020-02-18 16:44:44 +0100

    tableau jquery home admin

--- tp_propar/view/homeAdmin.php
+++ tp_propar/view/homeAdmin.php
@@ -41,8 +41,20 @@ if ($_SESSION['type'] != 'EXPERT') {
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" ></script>
+  <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
+  <script>
+  $(document).ready(function() {
+    $('#currentOp').DataTable();
+} ); </script>
+<script>
+  $(document).ready(function() {
+    $('#finishOp').DataTable();
+} ); </script>
+
   <!-- HEADER -->
   <div class="container-fluid">
     <div class="row d-flex justify-content-between header">
@@ -109,7 +121,7 @@ if ($_SESSION['type'] != 'EXPERT') {
 
           <div class="row d-flex justify-content-center">
             <div class="col-md-12">
-              <table class="table table-hover">
+              <table id ="currentOp" class="table table-hover">
                 <thead class="thead-dark">
                   <tr>
                     <th class="text-left"><strong class="font-weight-bold font-italic">ID Opération</strong></th>
@@ -154,7 +166,7 @@ if ($_SESSION['type'] != 'EXPERT') {
 
           <div class="row d-flex justify-content-center">
             <div class="col-md-12">
-              <table class="table table-hover">
+              <table id="finishOp" class="table table-hover">
                 <thead class="thead-dark">
                   <tr>
                     <th class="text-left"><strong class="font-weight-bold font-italic">ID Opération</strong></th>
